12be6a6e0ee0b34a5e2f3ab409bb063d7cc4bb64
Gioppyy <75016084+Gioppyy@users.noreply.github.com>
2024-04-07 18:32:29 +0200

    Fixed post link in home page

--- test_create_blogpost.py
+++ test_create_blogpost.py
@@ -3,8 +3,8 @@ from requests import post
 URL = "http://127.0.0.1:5000/api/v1/createblogpost"
 
 data = {
-    "title": "Quanto è inquinata la tua città?",
-    "subtitle": "Scopri quanto è inquinata l'aria che respiri",
+    "title": "Test test test",
+    "subtitle": "testino testino testino",
     "creator": "Gioppy"
 }
 
